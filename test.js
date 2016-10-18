3c4419776a933e48f135f815aa4b536c6c7e246d
Guntur <poetra.guntur@gmail.com>
2016-10-19 04:22:20 +0700

    test --dry-run

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 import p from 'path';
 import test from 'ava';
 import fs from 'fs-extra';
+import execa from 'execa';
 import pathExists from 'path-exists';
 
 global.Promise = Promise;
-const execa = require('execa');
 
 const fixtures = ['bar.txt', 'foo.txt', '.dot'];
 
 test.beforeEach(t => {
-	t.context.tmp = p.resolve('', '.tmp');
+	t.context.tmp = p.resolve('', p.join('.tmp', Math.random().toString().substring(2, 5)));
 	t.context.files = p.join(t.context.tmp, 'files');
 	t.context.symlinks = p.join(t.context.tmp, 'symlinks');
 
@@ -20,11 +20,19 @@ test.beforeEach(t => {
 });
 
 test('remove', async t => {
-	const res = await execa('./cli.js', fixtures.map(fl => p.join(t.context.symlinks, fl)));
+	await execa('./cli.js', fixtures.map(fl => p.join(t.context.symlinks, fl)));
 
-	t.notDeepEqual(res, fixtures);
 	fixtures.forEach(f => {
 		t.true(pathExists.sync(p.join(t.context.files, f)));
 		t.false(pathExists.sync(p.join(t.context.symlinks, f)));
+	});
+});
+
+test('--dry-run', async t => {
+	await execa('./cli.js', ['--dry-run'].concat(fixtures.map(fl => p.join(t.context.symlinks, fl))));
+
+	fixtures.forEach(f => {
+		t.true(pathExists.sync(p.join(t.context.files, f)));
+		t.true(pathExists.sync(p.join(t.context.symlinks, f)));
 	});
 });
